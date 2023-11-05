6b921a3abefd76afa55844cf3bf70d9e2d3c8782
kz_morita <kz.morit@gmail.com>
2023-11-05 16:44:16 +0900

    Add search api

--- book-api/src/main.rs
+++ book-api/src/main.rs
@@ -1,9 +1,9 @@
-use axum::response::IntoResponse;
-use serde::Serialize;
-use std::net::SocketAddr;
-
+use axum::http::StatusCode;
+use axum::response::{IntoResponse, Response};
 use axum::routing::get;
 use axum::{Json, Router};
+use serde::{Deserialize, Serialize};
+use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
@@ -22,24 +22,100 @@ async fn main() {
 
 #[derive(Serialize)]
 struct BookResponse {
-    isbn: String,
+    items: Vec<BookItemResponse>,
+}
+
+#[derive(Serialize)]
+struct BookItemResponse {
+    id: String,
+    isbn10: Option<String>,
+    isbn13: Option<String>,
     title: String,
-    image_url: String,
+}
+
+struct AppError(anyhow::Error);
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!("Internal Server Error: {}", self.0),
+        )
+            .into_response()
+    }
+}
+impl<E> From<E> for AppError
+where
+    E: Into<anyhow::Error>,
+{
+    fn from(err: E) -> Self {
+        Self(err.into())
+    }
 }
 
 async fn index() -> &'static str {
     "OK"
 }
 
-async fn search() -> impl IntoResponse {
-    let resp = search_book_by_title().await;
-    Json(resp)
+async fn search() -> Result<Json<BookResponse>, AppError> {
+    let resp = search_book_by_title("データ指向".to_owned()).await?;
+    Ok(Json(resp))
 }
 
-async fn search_book_by_title() -> BookResponse {
-    BookResponse {
-        isbn: "xxxxxxxxxx".to_owned(),
-        title: "sample title".to_owned(),
-        image_url: "https://example.com/images/1.png".to_owned(),
-    }
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct BookAPIResponse {
+    total_items: i64,
+    items: Vec<BookItem>,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct BookItem {
+    id: String,
+    volume_info: BookVolumeInfo,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct BookVolumeInfo {
+    title: String,
+    industry_identifiers: Vec<BookIndustryIdentifier>,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct BookIndustryIdentifier {
+    r#type: String,
+    identifier: String,
+}
+
+async fn search_book_by_title(search_query: String) -> anyhow::Result<BookResponse> {
+    let req_url = format!("https://www.googleapis.com/books/v1/volumes?q={search_query}");
+    let res = reqwest::get(req_url).await?;
+
+    let body: BookAPIResponse = res.json().await?;
+    println!("Body\n{:?}", body);
+
+    let items: Vec<BookItemResponse> = body
+        .items
+        .into_iter()
+        .map(|item: BookItem| {
+            let mut isbn_iterator = item.volume_info.industry_identifiers.into_iter();
+            let isbn13: Option<String> = isbn_iterator
+                .find(|i| i.r#type == "ISBN_13")
+                .map(|i| i.identifier.to_owned());
+            let isbn10: Option<String> = isbn_iterator
+                .find(|i| i.r#type == "ISBN_10")
+                .map(|i| i.identifier.to_owned());
+
+            BookItemResponse {
+                id: item.id,
+                isbn10: isbn10.to_owned(),
+                isbn13: isbn13.to_owned(),
+                title: item.volume_info.title,
+            }
+        })
+        .collect();
+
+    Ok(BookResponse { items })
 }
